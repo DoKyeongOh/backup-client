f7fd1d073b3916bdb704db9a05a959cb28cf13de
DoKyeong Oh <dkproh@gmail.com>
2023-01-04 00:36:26 +0900

    feat : 메이븐 + 스프링 부트 프로젝트 생성

---
+++ src/main/java/com/inspien/backupclientspring/BackupClientSpringApplication.java
@@ -0,0 +1,13 @@
+package com.inspien.backupclientspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupClientSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupClientSpringApplication.class, args);
+	}
+
+}
