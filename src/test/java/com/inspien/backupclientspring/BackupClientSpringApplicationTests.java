802e74cb56eb09da7ec0f8278e4361f3eb2856e0
DoKyeong Oh <dkproh@gmail.com>
2023-01-04 00:58:06 +0900

    chore : minor (신경쓰지 않으셔도 됩니다)

---
+++ src/test/java/com/inspien/backupclientspring/BackupClientSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspien.backupclientspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupClientSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
